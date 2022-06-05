da562b3f13e97a11b4e51964f27761008cbf0c9e
webtechromanov <66905718+webtechromanov@users.noreply.github.com>
2022-06-05 13:37:27 +0300

    Addint constructor

--- Assets/Scripts/EnemyInstantiate.cs
+++ Assets/Scripts/EnemyInstantiate.cs
@@ -8,10 +8,20 @@ public class EnemyInstantiate : MonoBehaviour
     [SerializeField] private int _iterationsAmount = 3;
 
     private Coroutine _enemyCreation;
-    private static Vector3 _enemyPosition1 = new Vector3(-5, -3.5f);
-    private static Vector3 _enemyPosition2 = new Vector3(0, 4);
-    private static Vector3 _enemyPosition3 = new Vector3(5, -3.5f);
-    private Vector3[] _spawnPositions = new Vector3[3] { _enemyPosition1, _enemyPosition2, _enemyPosition3 };
+    private Vector3 _enemyPosition1 = new Vector3(-5, -3.5f);
+    private Vector3 _enemyPosition2 = new Vector3(0, 4);
+    private Vector3 _enemyPosition3 = new Vector3(5, -3.5f);
+    private Vector3[] _spawnPositions;
+
+    public EnemyInstantiate()
+    {
+        _spawnPositions = new Vector3[3]
+        {
+            _enemyPosition1,
+            _enemyPosition2,
+            _enemyPosition3
+        };
+    }
 
     private void Start()
     {
